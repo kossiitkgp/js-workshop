dab2f7c1b630d41a638dc080cc1bb8f096d13f4c
piyush27ranjan <piyush.27ranjan@gmail.com>
2019-01-19 01:59:31 +0530

    Added REST material

---
+++ backend.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.headers);
+    if (req.method = 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
